4089017136c8572194c098a5a62dc649a6a363be
Kimberli Smith <kimsmith.it@gmail.com>
2016-09-16 22:16:39 -0400

    initial commit

---
+++ src/LetterGrades.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+/**
+ * 
+ */
+
+/**
+ * @author Kimberli
+ *
+ */
+public class LetterGrades {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	
+		int num = 0;
+		String grade = null;
+		String choice = "y";
+		
+		//output and prompts
+		System.out.println("Grade Converter!");
+		System.out.println();
+	
+		Scanner k = new Scanner(System.in);
+	
+		Methods.getGrade(num, choice, grade);
+	
+
+		
+	}//psvm
+
+}//end class letter grades
---
+++ src/Methods.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+/**
+ * 
+ */
+
+/**
+ * @author Kimberli
+ *
+ */
+public class Methods {
+
+	/**
+	 * @param grade 
+	 * @param args
+	 */
+
+	public static void getGrade(int num, String choice, String grade ) {
+		Scanner k = new Scanner(System.in);
+		do {
+		System.out.println("Enter a numeric grade:  ");
+		 num = k.nextInt();
+			if(num<=60){
+				System.out.println("Letter Grade:  F ");
+			}else if(num > 60 && num < 67){
+				System.out.println("Letter Grade:  D");
+			}else if(num > 66 && num < 80){
+				System.out.println("Letter Grade:  C");
+			}else if(num> 79 && num < 88 ){
+				System.out.println("Letter Grade:  B");
+			}else{
+				System.out.println("Letter Grade:  A");
+					}
+			System.out.println("Continue? y/n");
+			k.nextLine();
+			choice = k.nextLine();
+			}while(choice.equals("y"));
+			}
+	}
